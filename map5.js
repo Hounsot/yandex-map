ef0526fb2f1484928ae15cbd7fc2d58f577f6b68
Matvey Vasilyev <64931129+Hounsot@users.noreply.github.com>
2025-07-28 10:37:54 +0300

    Update map5.js

--- map5.js
+++ map5.js
@@ -25,7 +25,7 @@ let map = null;
             const MARKER_COORDINATES = [37.194674, 55.565233];
             const LOCATION = {
                 center: CENTER_COORDINATES,
-                zoom: 20
+                zoom: 10
             };
 
             // Initialize map
